cfadce56c2caed89b938bfe8cf839e4bf1ed7acf
pistachi73 <oscarpulido98@gmail.com>
2024-06-16 12:10:12 +0200

    fix build issues

--- src/app/(app)/shots/page.tsx
+++ src/app/(app)/shots/page.tsx
@@ -1,7 +1,5 @@
-import { ShotCarousel } from "@/components/shots/shot-carousel";
-
 const ShotsPage = () => {
-  return <ShotCarousel currentShot={0} />;
+  return <div>hello</div>;
 };
 
 export default ShotsPage;
